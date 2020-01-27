2110d41705629419ea355e611d52e18b2ab917d0
mark100net <mark@pokervane.com>
2020-01-26 20:39:37 -0800

    wip: update deploy to push

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,8 @@
 echo "The push will go here"
 docker ps
 docker images
+echo "username:"
+echo $DOCKER_USERNAME
+docker login babywearing.azurecr.io --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
+docker push babywearing.azurecr.io/babywearing:prod
 
