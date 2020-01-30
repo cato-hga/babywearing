81f1c3a2c940d1b14228b9d3602ee9bc4830132e
mark100net <mark@pokervane.com>
2020-01-30 13:12:23 -0800

    consolidate deploy script

--- deploy/before_deploy.sh
+++
@@ -1,2 +0,0 @@
-echo $TRAVIS_COMMIT > ./deploy/deploy_hash
-docker build -f Dockerfile.prod -t babywearing.azurecr.io/babywearing:prod .
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,8 @@
 echo "The push will go here"
+set -ev
 
+echo $TRAVIS_COMMIT > ./deploy/deploy_hash
+docker build -f Dockerfile.prod -t babywearing.azurecr.io/babywearing:prod .
 docker ps
 docker images
 docker login babywearing.azurecr.io --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
