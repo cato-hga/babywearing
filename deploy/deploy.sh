ba49030aa1074c1fb07a9e78cadc1e14eb4c4ff5
mark100net <mark@pokervane.com>
2020-01-30 14:12:09 -0800

    wip

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,7 @@ set -ev
 
 echo $TRAVIS_COMMIT > ./deploy/deploy_hash
 echo $MASTER_KEY > ./config/master.key
+cat ./config/master.key
 
 docker build -f Dockerfile.prod -t babywearing.azurecr.io/babywearing:prod .
 docker ps
