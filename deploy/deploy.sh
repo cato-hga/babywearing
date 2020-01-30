fbba7141aa28369ecddd8f39855893350f109f9c
mark100net <mark@pokervane.com>
2020-01-30 07:07:54 -0800

    show travis commit var

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,7 @@
 echo "The push will go here"
+echo $TRAVIS_COMMIT
 docker ps
 docker images
-echo "username:"
-echo $DOCKER_USERNAME
 docker login babywearing.azurecr.io --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 docker push babywearing.azurecr.io/babywearing:prod
 
